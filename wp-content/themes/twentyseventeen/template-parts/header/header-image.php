4258b8deaca22650e2d7cfa71fba86b60b231b03
lonw <admin@lonw.com>
2017-03-30 15:14:55 -0400

    first commit

---
+++ wp-content/themes/twentyseventeen/template-parts/header/header-image.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Displays header media
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="custom-header">
+
+	<div class="custom-header-media">
+		<?php if(page_id==2){}else{the_custom_header_markup();}; ?>
+
+		
+		
+		
+	</div>
+
+	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
+
+</div><!-- .custom-header -->
